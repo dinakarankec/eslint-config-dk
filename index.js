3da27c77f98790121293f6a220451b90b66ac5f5
dinakarankec <dinakaran.santhanam@vivriticapital.com>
2020-05-27 15:29:36 +0530

    fix: reverting to older config

--- index.js
+++ index.js
@@ -1,79 +1,39 @@
 module.exports = {
-  env: {
-    browser: true,
-    es6: true,
-    node: true,
-    jest: true,
-  },
-  parser: "@typescript-eslint/parser",
+  parser: "@typescript-eslint/parser", // Specifies the ESLint parser
   extends: [
-    "airbnb",
-    "plugin:react/recommended",
-    "plugin:@typescript-eslint/recommended",
-    "prettier/@typescript-eslint",
-    "plugin:prettier/recommended",
+    "plugin:@typescript-eslint/recommended", // Uses the recommended rules from @typescript-eslint/eslint-plugin
     "react-app",
+    "prettier/@typescript-eslint", // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
+    "plugin:prettier/recommended",
+    // "plugin:jsx-a11y/recommended"
+  ],
+  plugins: [
+    "@typescript-eslint",
+    "react",
     "react-hooks",
+    "prettier",
+    "import" /*"jsx-a11y"*/,
   ],
   parserOptions: {
-    ecmaVersion: 2018,
-    sourceType: "module",
+    ecmaVersion: 2018, // Allows for the parsing of modern ECMAScript features
+    sourceType: "module", // Allows for the use of imports
     ecmaFeatures: {
-      jsx: true,
+      jsx: true, // Allows for the parsing of JSX
     },
   },
   rules: {
-    // "indent": [2, 4],
-    "react/jsx-filename-extension": [0],
+    "@typescript-eslint/explicit-function-return-type": 0,
+    "@typescript-eslint/no-explicit-any": 0,
+    "@typescript-eslint/interface-name-prefix": ["error", "always"],
     "react-hooks/rules-of-hooks": "error",
     "react-hooks/exhaustive-deps": "warn",
-    "import/extensions": "off",
-    "max-len": [
-      "error",
-      {
-        code: 150,
-      },
-    ],
-    "no-use-before-define": [
-      2,
-      {
-        functions: false,
-      },
-    ],
-    "@typescript-eslint/camelcase": [
-      "error",
-      {
-        properties: "never",
-      },
-    ],
-    "react/jsx-indent": [2, 4],
-    "react/prop-types": [0],
-    "react/jsx-indent-props": [2, 4],
-    "jsx-a11y/label-has-for": [
-      2,
-      {
-        components: ["Label"],
-        required: {
-          some: ["nesting", "id"],
-        },
-        allowChildren: false,
-      },
-    ],
-
-    "jsx-a11y/anchor-is-valid": [
-      "error",
-      {
-        components: ["Link"],
-        specialLink: ["to"],
-      },
-    ],
+    "@typescript-eslint/no-empty-function": 0,
+    // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
+    // e.g. "@typescript-eslint/explicit-function-return-type": "off",
   },
   settings: {
-    "import/resolver": {
-      node: {
-        moduleDirectory: ["node_modules", "src/"],
-        extensions: [".js", ".jsx", ".ts", ".tsx"],
-      },
+    react: {
+      version: "detect", // Tells eslint-plugin-react to automatically detect the version of React to use
     },
   },
 };
